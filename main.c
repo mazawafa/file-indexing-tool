b3364e6a604ba4da7c0f127c2d652061fa1401b3
Evgeny Mazin <116568732+chuvachill@users.noreply.github.com>
2025-11-22 20:14:20 +0300

    Add reading directory content

--- main.c
+++ main.c
@@ -1,16 +1,35 @@
+#include <dirent.h> // readdir()
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>     // getcwd()
+#include <unistd.h> // getcwd()
 
-int main(int argc, char* argv[]) { 
+int main(int argc, char* argv[]) {
     char* dname = NULL;
-    
+
     if ((dname = getcwd(dname, 0)) == NULL) {
         fprintf(stderr, "error getting current working directory\n");
         return EXIT_FAILURE;
     }
 
-    printf("%s\n", dname);
-    free(dname); 
+    DIR* dp;
+    if ((dp = opendir(dname)) == NULL) {
+        fprintf(stderr, "error opening %s\n", dname);
+        free(dname);
+        return EXIT_FAILURE;
+    }
+
+    struct dirent* ent;
+    errno = 0;
+    while ((ent = readdir(dp)) != NULL) {
+        printf("%s\n", ent->d_name);
+    }
+    if (ent == NULL && errno) {
+        perror("readdir");
+        free(dname);
+        return EXIT_FAILURE;
+    }
+
+    free(dname);
     return EXIT_SUCCESS;
 }
